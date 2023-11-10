2c5e332f5eb91231860e20555bb64b8584f4f9f4
MAGNANT Leo <leo.magnant@ynov.com>
2023-11-10 16:18:41 +0100

    models added

---
+++ src/models/album.models.js
@@ -0,0 +1,12 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const AlbumSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  artist: { type: Mongoose.Schema.Types.ObjectId, ref:'Artist' , required: false },
+  songs: [{ type: Mongoose.Schema.Types.ObjectId, ref:'Song' }],
+  converImage : { type: String, required: false },
+});
+
+const Album = mongoose.model('Album', AlbumSchema);
+
+export default Album;
---
+++ src/models/artist.models.js
@@ -0,0 +1,12 @@
+// models/User.js
+import mongoose from 'mongoose';
+
+const artistSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    lastName: { type: String, required: true, unique: true },
+    albums: [{ type: Mongoose.Schema.Types.ObjectId, ref:'Albums' , required: false }],
+});
+
+const Artist = mongoose.model('Artist', artistSchema);
+
+export default Artist;
