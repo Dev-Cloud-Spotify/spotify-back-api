87d28fc343aef83b5179fb9342cc71bc5a89b1a1
ThomasJanko <thomas78125@gmail.com>
2023-11-10 15:07:56 +0100

    connection MongoDB + start server

--- src/index.js
+++ src/index.js
@@ -1,25 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+const colors = require('@colors/colors');
+
 app.use(bodyParser.json());
 
+
+const cluster = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/your-database-name', {});
+mongoose.connect(cluster);
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
-  console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB'.cyan);
 });
 
 const router = require('./routes/router');
 app.use('/api', router);
 
+// ASCII art 'Spotify API'
+const spotify = `
+ ██████╗██████╗  █████╗ ████████╗██╗███████╗██╗   ██╗
+██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██║██╔════╝╚██╗ ██╔╝
+╚█████╗ ██████╔╝██║  ██║   ██║   ██║█████╗   ╚████╔╝ 
+ ╚═══██╗██╔═══╝ ██║  ██║   ██║   ██║██╔══╝    ╚██╔╝
+██████╔╝██║     ╚█████╔╝   ██║   ██║██║        ██║
+╚═════╝ ╚═╝      ╚════╝    ╚═╝   ╚═╝╚═╝        ╚═╝
+`;
+
+const API=`
+ █████╗ ██████╗ ██╗
+██╔══██╗██╔══██╗██║
+███████║██████╔╝██║
+██╔══██║██╔═══╝ ██║
+██║  ██║██║     ██║
+╚═╝  ╚═╝╚═╝     ╚═╝
+`
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`.magenta);
+  console.log(spotify.green)
+  console.log(API.yellow)
 });
