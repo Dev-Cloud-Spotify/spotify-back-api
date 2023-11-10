b22123702d9cfb99e527beeedb6a3e952539f99f
ThomasJanko <thomas78125@gmail.com>
2023-11-10 16:55:02 +0100

    all routes + all controllers

--- src/routes/song.routes.js
+++ src/routes/song.routes.js
@@ -3,5 +3,10 @@ const router = express.Router();
 import songController from '../controllers/song.controllers';
 
 router.get('/test', songController.test);
+router.post('/createSong', songController.createSong);
+router.get('/getSongs', songController.getSongs);
+router.get('/getSongById/:id', songController.getSongById);
+router.put('/updateSongById/:id', songController.updateSongById);
+router.delete('/deleteSongById/:id', songController.deleteSongById);
 
 export default router;
