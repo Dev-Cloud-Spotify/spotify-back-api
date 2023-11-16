9d695a3f667a1416ed926af8513f031911e3e79f
MAGNANT Leo <leo.magnant@ynov.com>
2023-11-16 16:49:25 +0100

    Correction routes

--- src/models/song.models.js
+++ src/models/song.models.js
@@ -1,23 +1,16 @@
+// models/song.models.js
 import mongoose from 'mongoose';
 
 const songSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  autor: { type: String, required: true },
+  autor: { type: String, required:true},
   date_out: { type: Date, required: true },
-  url: { type: String, required: false },
-  album: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Album',
-    required: false,
-  },
-  artist: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Artist',
-    required: false,
-  },
-  listens: { type: Number, default: 0 },
+  url: { type: String, required:false},
+  album: { type: mongoose.Schema.Types.ObjectId, ref:'Album' , required: false },
+  artist: { type: mongoose.Schema.Types.ObjectId, ref:'Artist' , required: false },
+  listens: { type: Number, default: 0},
 });
 
-const Song = mongoose.model('songs', songSchema);
+const Song = mongoose.model('Song', songSchema);
 
 export default Song;
--- src/routes/router.js
+++ src/routes/router.js
@@ -7,9 +7,11 @@ const router = express.Router();
 import songRoutes from './song.routes';
 import albumRoutes from './album.routes';
 import artistRoutes from './artist.routes';
+import userRoutes from './user.routes';
 
 router.use('/songs', songRoutes);
 router.use('/albums', albumRoutes);
 router.use('/artists', artistRoutes);
+router.use('/users', userRoutes);
 
 export default router;
